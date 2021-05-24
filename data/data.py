c9cb9c7a649ba2c5933beb38cdd288ba38a9295b
CBurger <c_burger@gmx.at>
2021-05-24 18:39:16 +0200

    feat: added colours and dummy sizes  for plotting of the celestial bodies

--- data/data.py
+++ data/data.py
@@ -6,35 +6,42 @@ names = ["Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranu
 # the masses of the individual celestial bodies
 mass = [1.0000e00, 1.6600e-7, 2.4476e-6, 3.0032e-6, 3.2268e-7, 9.5425e-4, 2.8572e-4, 4.3643e-5, 5.1486e-5, 6.6060e-9]
 
+# dummy sizes for plotting
+dummy_sizes = [25, 7, 7, 7, 7, 7, 7, 7, 7, 7]
+
+# the colours of the individual celestial bodies
+colour = ["#fffd37", "#acc2d9", "#f0833a", "#03719c", "#fdaa48",
+          "#e2ca76", "#ffa756", "#d0fefe", "#069af3", "#ffd8b1"]
+
 # graviational constant
 G = 2.959122e-4
 
 # the positions [x, y, z] of the individual celestial bodies
 position = [
-	[-1.6132380e-03, -2.3674938e-03, -3.4999903e-05],	# /*Soleil*/
-	[3.4942761e-01, 1.0619088e-02, -3.1182970e-02], 	# /*Mercure*/
-	[-5.7685710e-01, 4.2639983e-01, 3.9039256e-02],		# /*Venus*/
-	[6.8900355e-01, 7.0799513e-01, -5.4762805e-05],		# /*Terre*/
-	[4.3534005e-01, -1.3548686e+00, -3.9100527e-02],	# /*Mars*/
-	[1.8147606e+00, 4.7059241e+00, -6.0234633e-02],		# /*Jupiter*/
-	[-8.2319838e+00, -5.2651142e+00, 4.1915711e-01],	# /*Saturn*/
-	[1.9916762e+01, 2.3700466e+00, -2.4922728e-01],		# /*Uranus*/
-	[2.6481558e+01, -1.4076556e+01, -3.2040642e-01],	#/*Neptune*/
-	[4.9371124e+00, -3.1890424e+01, 1.9843679e+00],		#/*Pluto*/
+    [-1.6132380e-03, -2.3674938e-03, -3.4999903e-05],  # /*Soleil*/
+    [3.4942761e-01, 1.0619088e-02, -3.1182970e-02],  # /*Mercure*/
+    [-5.7685710e-01, 4.2639983e-01, 3.9039256e-02],  # /*Venus*/
+    [6.8900355e-01, 7.0799513e-01, -5.4762805e-05],  # /*Terre*/
+    [4.3534005e-01, -1.3548686e+00, -3.9100527e-02],  # /*Mars*/
+    [1.8147606e+00, 4.7059241e+00, -6.0234633e-02],  # /*Jupiter*/
+    [-8.2319838e+00, -5.2651142e+00, 4.1915711e-01],  # /*Saturn*/
+    [1.9916762e+01, 2.3700466e+00, -2.4922728e-01],  # /*Uranus*/
+    [2.6481558e+01, -1.4076556e+01, -3.2040642e-01],  # /*Neptune*/
+    [4.9371124e+00, -3.1890424e+01, 1.9843679e+00],  # /*Pluto*/
 ]
 
 # the velocity [v_x, v_y, v_z] of the individual celestial bodies
 deltav = [
-	[6.0453208e-06, -1.8980631e-06, -1.3060634e-07],		# /*Soleil*/
-	[-6.4674481e-03, 2.9372684e-02, 2.9939160e-03],			# /*Mercure*/
-	[-1.2159929e-02, -1.6321487e-02, 4.7839154e-04],		# /*Venus*/
-	[-1.2606830e-02, 1.1932472e-02, -5.3326343e-07],		# /*Terre*/
-	[1.3851438e-02, 5.5002850e-03, -2.2479471e-04],			# /*Mars*/
-	[-7.1329432e-03, 3.0767709e-03, 1.4683761e-04],			# /*Jupiter*/
-	[2.7027945e-03, -4.7129500e-03, -2.5483556e-05],		# /*Saturn*/
-	[-4.9354675e-04, 3.7222065e-03, 2.0227673e-05],			# /*Uranus*/
-	[1.4527105e-03, 2.7901455e-03, -9.1342324e-05],			# /*Neptune*/
-	[3.1711953e-03, -1.4100225e-04, -9.0807325e-04]			# /*Pluto*/
+    [6.0453208e-06, -1.8980631e-06, -1.3060634e-07],  # /*Soleil*/
+    [-6.4674481e-03, 2.9372684e-02, 2.9939160e-03],  # /*Mercure*/
+    [-1.2159929e-02, -1.6321487e-02, 4.7839154e-04],  # /*Venus*/
+    [-1.2606830e-02, 1.1932472e-02, -5.3326343e-07],  # /*Terre*/
+    [1.3851438e-02, 5.5002850e-03, -2.2479471e-04],  # /*Mars*/
+    [-7.1329432e-03, 3.0767709e-03, 1.4683761e-04],  # /*Jupiter*/
+    [2.7027945e-03, -4.7129500e-03, -2.5483556e-05],  # /*Saturn*/
+    [-4.9354675e-04, 3.7222065e-03, 2.0227673e-05],  # /*Uranus*/
+    [1.4527105e-03, 2.7901455e-03, -9.1342324e-05],  # /*Neptune*/
+    [3.1711953e-03, -1.4100225e-04, -9.0807325e-04]  # /*Pluto*/
 ]
 
 #
@@ -49,48 +56,45 @@ deltav = [
 # double v_K3[10][3] = {{0., 0., 0.},{0., 0., 0.},{0., 0., 0.},{0., 0., 0.},{0., 0., 0.},{0., 0., 0.},{0., 0., 0.},{0., 0., 0.},{0., 0., 0.},{0., 0., 0.}};
 
 
-#Soleil
-psi_obs_Soleil = 0.				# Seulement dans un but de cohérence
+# Soleil
+psi_obs_Soleil = 0.  # Seulement dans un but de cohérence
 psi_th_Soleil = 0.
 
-#Mercure
+# Mercure
 psi_obs_Mercure = 5.75
 psi_th_Mercure = 5.50
 
-#Venus
+# Venus
 psi_obs_Venus = 2.04
 psi_th_Venus = 10.75
 
-#Terre
+# Terre
 psi_obs_Terre = 11.45
 psi_th_Terre = 11.87
 
-#Mars
+# Mars
 psi_obs_Mars = 16.28
 psi_th_Mars = 17.60
 
-#Jupiter
+# Jupiter
 psi_obs_Jupiter = 6.55
 psi_th_Jupiter = 7.42
 
-#Saturn
+# Saturn
 psi_obs_Saturn = 19.5
 psi_th_Saturn = 18.36
 
-#Uranus
+# Uranus
 psi_obs_Uranus = 3.34
 psi_th_Uranus = 2.72
 
-#Neptune
+# Neptune
 psi_obs_Neptune = 0.36
 psi_th_Neptune = 0.65
 
-#Pluton
-psi_obs_Pluton = 0.				# On ne sait pas
+# Pluton
+psi_obs_Pluton = 0.  # On ne sait pas
 psi_th_Pluton = 0.
 
-
-
-#TOTAL
+# TOTAL
 m_totale = 1.0000e00 + 1.6600e-7 + 2.4476e-6 + 3.0032e-6 + 3.2268e-7 + 9.5425e-4 + 2.8572e-4 + 4.3643e-5 + 5.1486e-5 + 6.6060e-9
-
--- utils/utils.py
+++ utils/utils.py
@@ -1,20 +1,33 @@
+import numpy as np
+
+
 class CelestialBody:
 
-    def __init__(self, name, mass, x_pos, y_pos, z_pos, x_deltav, y_deltav, z_deltav):
+    def __init__(self, name, mass, dummy_size, colour, x_pos, y_pos, z_pos, x_deltav, y_deltav, z_deltav, x_acc, y_acc, z_acc):
         self.name = name
         self.mass = mass
+        self.dummy_size = dummy_size
+        self.colour = colour
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.z_pos = z_pos
         self.x_deltav = x_deltav
         self.y_deltav = y_deltav
         self.z_deltav = z_deltav
+        self.x_acc = x_acc
+        self.y_acc = y_acc
+        self.z_acc = z_acc
+        self.time = 0
 
     def hello(self):
         return "Hello, {}".format(self.name)
 
     def position(self):
-        return [self.x_pos, self.y_pos, self.z_pos]
+        return np.array([self.x_pos, self.y_pos, self.z_pos])
 
     def velocity(self):
-        return [self.x_deltav, self.y_deltav, self.z_deltav]
+        return np.array([self.x_deltav, self.y_deltav, self.z_deltav])
+
+    def acceleration(self):
+        return np.array([self.x_acc, self.y_acc, self.z_acc])
+
